21bf5b192a13991ee5a9340bb5a438c8d0f2583e
AlpineIce <cholgate322@gmail.com>
2024-12-11 19:42:07 -0500

    Update shader compile file

--- example/ShaderCompile.py
+++ example/ShaderCompile.py
@@ -15,11 +15,11 @@ def compile_shader(src_path, dst_path):
 
 #then compile shaders
 pool = concurrent.futures.ThreadPoolExecutor(max_workers=6)
-pool.submit(compile_shader, "./shaders/raytrace.rchit", "../build/resources/shaders/raytrace_chit.spv")
-pool.submit(compile_shader, "./shaders/raytrace.rgen", "../build/resources/shaders/raytrace_rgen.spv")
-pool.submit(compile_shader, "./shaders/raytrace.rmiss", "../build/resources/shaders/raytrace_rmiss.spv")
-pool.submit(compile_shader, "./shaders/raytraceShadow.rmiss", "../build/resources/shaders/raytraceShadow_rmiss.spv")
-pool.submit(compile_shader, "./shaders/Default.vert", "../build/resources/shaders/Default_vert.spv")
-pool.submit(compile_shader, "./shaders/Default.frag", "../build/resources/shaders/Default_frag.spv")
+pool.submit(compile_shader, "./resources/shaders/raytrace.rchit", "../build/resources/shaders/raytrace_chit.spv")
+pool.submit(compile_shader, "./resources/shaders/raytrace.rgen", "../build/resources/shaders/raytrace_rgen.spv")
+pool.submit(compile_shader, "./resources/shaders/raytrace.rmiss", "../build/resources/shaders/raytrace_rmiss.spv")
+pool.submit(compile_shader, "./resources/shaders/raytraceShadow.rmiss", "../build/resources/shaders/raytraceShadow_rmiss.spv")
+pool.submit(compile_shader, "./resources/shaders/Default.vert", "../build/resources/shaders/Default_vert.spv")
+pool.submit(compile_shader, "./resources/shaders/Default.frag", "../build/resources/shaders/Default_frag.spv")
 
 pool.shutdown(wait=True)
